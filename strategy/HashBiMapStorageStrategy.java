08d09b0d68aa493d08e85b1f7298f8927c9cba96
Nikita <tselishchevns@gmail.com>
2021-09-05 17:52:08 +0300

    .

---
+++ strategy/HashBiMapStorageStrategy.java
@@ -0,0 +1,28 @@
+package com.company.strategy;
+
+import com.google.common.collect.HashBiMap;
+
+public class HashBiMapStorageStrategy implements StorageStrategy {
+    private HashBiMap<Long, String> data = HashBiMap.create();
+
+    public boolean containsKey(Long key) {
+        return data.containsKey(key);
+    }
+
+    public boolean containsValue(String value) {
+        return data.containsValue(value);
+    }
+
+    @Override
+    public void put(Long key, String value) {
+        data.put(key, value);
+    }
+
+    public String getValue(Long key) {
+        return data.get(key);
+    }
+
+    public Long getKey(String value) {
+        return data.inverse().get(value);
+    }
+}
